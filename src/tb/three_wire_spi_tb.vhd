7564d876d1fbea9600621e63c05f0860fac5fedd
Joshua Edgcombe <joshedgcombe@gmail.com>
2022-10-28 02:09:34 -0400

    Clean up top level testbench, add CDC modules to .core file

--- src/tb/three_wire_spi_tb.vhd
+++ src/tb/three_wire_spi_tb.vhd
@@ -26,26 +26,18 @@ architecture sim of three_wire_spi_tb is
   -- Signals
   -----------------------------------------------------------------------------
   -- Control
-  signal rst_s : std_logic := '1';
-  signal clk_s : std_logic := '0';
-  signal axi_clk_s : std_logic := '0';
+  signal rst_s        : std_logic := '1';
+  signal clk_s        : std_logic := '0';
+  signal axi_clk_s    : std_logic := '0';
+  signal spi_sclk_s   : std_logic := '0';
 
   -- SPI
-  signal spi_sclk_s   : std_logic := '0';
   signal spi_sdio_s   : std_logic := 'Z';
   signal spi_csn_s    : std_logic;
-  -- signal spi_high_z_s : std_logic;
 
   -- Message
-  signal spi_trig_s : std_logic := '0';
-  signal spi_addr_s : std_logic_vector(NUM_ADDR_BITS_C-1 downto 0) := "1010110010011";
-  signal spi_data_s : std_logic_vector(NUM_DATA_BITS_C-1 downto 0) := x"AB";
-  -- signal spi_rw_s   : std_logic := '1';
-  signal spi_rw_s   : std_logic := '0';
-
-  signal spi_read_data_s  : std_logic_vector(NUM_DATA_BITS_C-1 downto 0);
-  signal spi_data_valid_s : std_logic;
-
+  signal spi_addr_s   : std_logic_vector(NUM_ADDR_BITS_C-1 downto 0) := "1010110010011";
+  signal spi_data_s   : std_logic_vector(NUM_DATA_BITS_C-1 downto 0) := x"AB";
 
   -- AXI
   signal s_axi_regs_awaddr_s  : std_logic_vector(AXI_REGS_ADDR_WIDTH_C-1 downto 0);
@@ -76,16 +68,6 @@ begin
 
   process
   begin
-    wait for 400 ns;
-    spi_trig_s    <= '1';
-    wait for CLK_25_PERIOD_C;
-    spi_trig_s    <= '0';
-    wait;
-  end process;
-
-
-  process
-  begin
     wait for 1.14 us;
     spi_sdio_s <= '1';
     wait for 5 ns;
@@ -110,34 +92,6 @@ begin
     finish;
     wait;
   end process;
-
-  -- u_dut: entity work.three_wire_spi(rtl)
-  --   generic map (
-  --     NUM_ADDR_BITS_G => NUM_ADDR_BITS_C,
-  --     NUM_DATA_BITS_G => NUM_DATA_BITS_C
-  --     )
-  --   port map (
-  --     -- Control lines
-  --     spi_clk_in_p         => clk_s,
-  --     spi_rst_in_p         => rst_s,
-
-  --     -- SPI interface
-  --     sclk_en_p            => spi_sclk_s,
-  --     sdio_in_p            => spi_sdio_s,
-  --     sdio_out_p           => spi_sdio_s,
-  --     sdio_high_z_p        => spi_high_z_s,
-  --     csn_p                => spi_csn_s,
-
-  --     -- Message lines
-  --     spi_trig_in_p        => spi_trig_s,
-  --     spi_addr_in_p        => spi_addr_s,
-  --     spi_rw_in_p          => spi_rw_s,
-  --     spi_data_in_p        => spi_data_s,
-
-  --     spi_data_out_p       => spi_read_data_s,
-  --     spi_data_valid_out_p => spi_data_valid_s
-  --     );
-
 
   u_dut: entity work.three_wire_spi_top(rtl)
     generic map (
