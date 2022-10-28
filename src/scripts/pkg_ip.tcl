117eeebd87711bcda17053a7429d4df117e90ec2
Joshua Edgcombe <joshedgcombe@gmail.com>
2022-10-28 19:43:42 -0400

    Clean up ip packaging script

--- src/scripts/pkg_ip.tcl
+++ src/scripts/pkg_ip.tcl
@@ -4,8 +4,8 @@
 set projName   "three-wire-spi"
 set partID     "xczu7ev-ffvc1156-2-e"
 set currentDir [file normalize .]
-set ipGenDir   "ip_tmp"
-set ipDir      "/home/midimaster21b/src/ip_repo"
+set ipGenDir   [file join $currentDir "ip_tmp"]
+set ipDir      [file join $currentDir "ip_repo"]
 set corePath   [file join $ipDir component.xml]
 set vendor     "midimaster21b"
 set library    "comm"
@@ -25,20 +25,15 @@ puts "================================================================";
 puts "Creating project \"$projName\" \[$partID\]";
 puts "================================================================";
 puts "Working directory: $currentDir";
-puts "Project directory: [file join $currentDir $ipDir]";
-
+puts "Project directory: $ipGenDir";
+puts "IP repo directory: $ipDir";
 
 
 create_project -force $projName $ipGenDir -part $partID
-
 set_property target_language VHDL [current_project]
-
 add_files -norecurse $fileList
-
 update_compile_order -fileset sources_1
-
 ipx::package_project -root_dir $ipDir -vendor $vendor -library $library -taxonomy $taxonomy -import_files -set_current false
-
 ipx::unload_core $corePath
 ipx::edit_ip_in_project -upgrade true -name tmp_edit_project -directory $ipDir $corePath
 update_compile_order -fileset sources_1
